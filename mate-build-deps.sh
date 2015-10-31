c4a04fa41f0bb6d520488c51ba30afbe286fc66e
ericfernandesferreira <candelabrus@gmail.com>
2015-10-31 19:20:19 -0200

    installation script

---
+++ mate-build-deps.sh
@@ -0,0 +1,104 @@
+#!/bin/sh
+
+# Copyright 2012  Patrick J. Volkerding, Sebeka, Minnesota, USA
+# All rights reserved.
+#
+# Copyright 2014 Willy Sudiarto Raharjo <willysr@slackware-id.org>
+# All rights reserved.
+#
+# Based on the xfce-build-all.sh script by Patrick J. Volkerding
+#
+# Redistribution and use of this script, with or without modification, is
+# permitted provided that the following conditions are met:
+#
+# 1. Redistributions of this script must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+#  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+#  EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Modified again by Eric Fernandes Ferreira <candelabrus@gmail.com> for my personal Mate Project
+
+# Set to 1 if you'd like to install/upgrade package as they are built.
+# This is recommended.
+INST=1
+
+# This is where all the compilation and final results will be placed
+TMP=${TMP:-/tmp}
+
+# This is the original directory where you started this script
+MSBROOT=$(pwd)
+
+# Loop for all dependency packages
+for dir in \
+  deps/vala \
+  deps/glade \
+  deps/vte3 \
+  deps/exempi \
+  deps/libgnomecanvas \
+  deps/libwnck3 \
+  deps/zenity \
+  deps/rarian \
+  deps/yelp-xsl \
+  deps/yelp-tools \
+  deps/libunique \
+  deps/libunique3 \
+  deps/pangox-compat \
+  deps/gtk-engines \
+  deps/murrine \
+  deps/pygobject3 \
+  deps/gtksourceview3 \
+  deps/libgtop \
+  deps/libgksu \
+  deps/gksu \
+  deps/gssdp \
+  deps/gupnp \
+  deps/libsigc++ \
+  deps/glibmm \
+  deps/cairomm \
+  deps/pangomm \
+  deps/atkmm \
+  deps/mm-common \
+  deps/gtkmm3 \
+  deps/perl-xml-twig \
+  deps/perl-net-dbus \
+  deps/system-tools-backends \
+  deps/liboobs \
+  deps/docutils \
+  ; do
+  # Get the package name
+  package=$(echo $dir | cut -f2- -d /)
+
+  # Change to package directory
+  cd $MSBROOT/$dir || exit 1
+
+  # Get the version
+  version=$(cat ${package}.SlackBuild | grep "VERSION:" | head -n1 | cut -d "-" -f2 | rev | cut -c 2- | rev)
+
+  # Get the build
+  build=$(cat ${package}.SlackBuild | grep "BUILD:" | cut -d "-" -f2 | rev | cut -c 2- | rev)
+
+  # The real build starts here
+  sh ${package}.SlackBuild || exit 1
+  if [ "$INST" = "1" ]; then
+    PACKAGE=$(ls $TMP/${package}-${version}-*-${build}*.txz 2>/dev/null)
+    if [ -f "$PACKAGE" ]; then
+      upgradepkg --install-new --reinstall "$PACKAGE"
+    else
+      echo "Error:  package to upgrade "$PACKAGE" not found in $TMP"
+      exit 1
+    fi
+  fi
+# mv $PACKAGE /home/backup/app/slackware/mate/1.11/install/deps
+# mv $PACKAGE /home/backup/app/slackware/mate/1.11/install-jack/deps
+  # back to original directory
+  cd $MSBROOT
+done
